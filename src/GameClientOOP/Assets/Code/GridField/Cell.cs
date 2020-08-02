f68b1ee6addd1d35bb1456126e654c832068a39c
Nick <mr.rabbiton@gmail.com>
2020-08-02 22:04:40 +0300

    Наработки по гриду

---
+++ src/GameClientOOP/Assets/Code/GridField/Cell.cs
@@ -0,0 +1,42 @@
+﻿namespace GridField
+{
+    public class Cell
+    {
+        public Cell(CellPosition position, CellInfo info)
+        {
+            _position = position;
+            _info = info;
+            _obj = null;
+        }
+
+        public CellObject GetCellObject() { return _obj; }
+        public void SetCellObject(CellObject obj) { _obj = obj; }
+        public CellInfo GetInfo() { return _info; }
+        public CellPosition GetPosition() { return _position; }
+
+        private CellObject _obj;
+        private CellInfo _info;
+        private CellPosition _position;
+    }
+
+    public class CellObject
+    {
+
+    }
+    public class CellInfo
+    {
+
+    }
+
+    public struct CellPosition
+    {
+        public CellPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int x;
+        public int y;
+    }
+}
